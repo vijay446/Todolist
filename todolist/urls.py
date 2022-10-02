d98b3380816ba21e1edd6981186c612161681cbd
Rohan <rohanpatel1552003@gmail.com>
2022-10-02 15:21:07 +0530

    Added form to capture data and implemented button functions

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,6 +1,11 @@
+from unicodedata import name
 from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('',views.index, name='index')
+    path('',views.index, name='index'),
+    path('add',views.addTodoItem,name='add'),
+    path('completed/<todo_id>',views.completedTodo,name='completed'),
+    path('deletecompleted',views.deleteCompleted,name='deletecompleted'),
+    path('deleteall',views.deleteAll,name='deleteall')
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -1,11 +1,38 @@
 from multiprocessing import context
-import re
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import TodolistForm
 from .models import Todolist
+from django.views.decorators.http import require_POST
 
 # Create your views here.
 def index(request):
     todo_items = Todolist.objects.order_by('id')
-    context = {'todo_items':todo_items}
+    form = TodolistForm()
+    context = {'todo_items':todo_items, 'form':form}
     return render(request,'todolist/index.html',context=context)
+
+@require_POST
+def addTodoItem(request):
+    form = TodolistForm(request.POST)
+
+    if form.is_valid():
+        new_todo = Todolist(text=request.POST['text'])
+        new_todo.save()
+
+    return redirect('index')
+
+def completedTodo(request,todo_id):
+    todo = Todolist.objects.get(pk=todo_id)
+    todo.completed = True
+    todo.save()
+
+    return redirect('index')
+
+def deleteCompleted(request):
+    Todolist.objects.filter(completed__exact = True).delete()
+
+    return redirect('index')
+
+def deleteAll(request):
+    Todolist.objects.all().delete()
+    return redirect('index')
